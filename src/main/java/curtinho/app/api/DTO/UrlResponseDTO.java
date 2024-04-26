2d0592072bb5f3e47ff518ee60304ef2a42bd0b5
Github-PMJG <56081679+JvDev-hash@users.noreply.github.com>
2024-04-26 09:39:17 -0300

    changes on controller and respDTO creation

---
+++ src/main/java/curtinho/app/api/DTO/UrlResponseDTO.java
@@ -0,0 +1,9 @@
+package curtinho.app.api.DTO;
+
+import lombok.Data;
+
+@Data
+public class UrlResponseDTO {
+
+    private String shortenUri;
+}
--- src/main/java/curtinho/app/api/controller/UrlController.java
+++ src/main/java/curtinho/app/api/controller/UrlController.java
@@ -1,7 +1,14 @@
 package curtinho.app.api.controller;
 
 import curtinho.app.api.DTO.UrlDTO;
+import curtinho.app.api.DTO.UrlResponseDTO;
+import curtinho.app.api.configuration.AuthorizationFilter;
 import curtinho.app.api.service.UrlService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -11,14 +18,22 @@ public class UrlController {
 
     private final UrlService urlService;
 
+    Logger logger = LoggerFactory.getLogger(UrlController.class);
+
     public UrlController(UrlService urlService) {
         this.urlService = urlService;
     }
 
     @PostMapping("s")
-    public String shortenUrl(@RequestBody UrlDTO urlDTO){
-
-        return urlService.shortenUrl(urlDTO.getLongUrl());
+    public ResponseEntity<?> shortenUrl(@RequestBody UrlDTO urlDTO){
+        var response = new UrlResponseDTO();
+        try {
+            response.setShortenUri(urlService.shortenUrl(urlDTO.getLongUrl()));
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e){
+            logger.error(e.getMessage());
+            return new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("p/{shortUri}")
